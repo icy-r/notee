c71b653b3c2e0ae167c8f924f1939781a22188f1
icy <asath12882@gmail.com>
2024-02-16 23:03:33 +0530

    Update analytics import in main.js

--- src/main.js
+++ src/main.js
@@ -3,26 +3,20 @@ import router from "./router";
 import App from "./App.vue";
 import { SpeedInsights } from "@vercel/speed-insights/vue"
 import { inject } from '@vercel/analytics';
-import firebase from './firebase';
+import analytics from './firebase';
 
 router.afterEach((to) => {
     // This will be triggered after every route change.
-    firebase.analytics().logEvent('page_view', {
+    analytics.logEvent('page_view', {
         page_path: to.path,
         page_location: window.location.href,
         page_title: to.name,
     });
 });
 
-
 const app = createApp(App);
 app.use(router);
 app.use(SpeedInsights);
 app.mount("#app");
 
-firebase.analytics();
-
 inject();
-
-
-
