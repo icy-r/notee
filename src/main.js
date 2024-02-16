a78cefc68cbbd8ccc10b3c2c1d91eacbec85b789
icy <asath12882@gmail.com>
2024-02-16 12:27:39 +0530

    Add analytics injection

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
 import { SpeedInsights } from "@vercel/speed-insights/vue"
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 const app = createApp(App);
 app.use(router);
