006ef0b5e6db99affc1ee3950e5dbc3509fb6530
icy <asath12882@gmail.com>
2024-02-16 22:57:33 +0530

    Add Firebase analytics and update dependencies

--- src/main.js
+++ src/main.js
@@ -3,11 +3,26 @@ import router from "./router";
 import App from "./App.vue";
 import { SpeedInsights } from "@vercel/speed-insights/vue"
 import { inject } from '@vercel/analytics';
- 
-inject();
+import firebase from './firebase';
+
+router.afterEach((to) => {
+    // This will be triggered after every route change.
+    firebase.analytics().logEvent('page_view', {
+        page_path: to.path,
+        page_location: window.location.href,
+        page_title: to.name,
+    });
+});
+
 
 const app = createApp(App);
 app.use(router);
 app.use(SpeedInsights);
 app.mount("#app");
+
+firebase.analytics();
+
+inject();
+
+
 
